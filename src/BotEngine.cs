56cbc79be922fd73e61808d657e8f86036d894ee
Mark-James McDougall <contact@markjames.dev>
2022-07-26 17:53:04 -0400

    - Added ListenForMessages() method. - Cleaned up program.cs

--- src/BotEngine.cs
+++ src/BotEngine.cs
@@ -15,11 +15,61 @@ namespace MetBot
             _botClient = new TelegramBotClient(accessToken);
         }
 
-        public async Task<User> CheckBotAsync()
+        // Create a listener so that we can wait for messages to be sent to the bot
+        public async Task ListenForMessages()
         {
-            var botInfo = await _botClient.GetMeAsync();
+            using var cts = new CancellationTokenSource();
 
-            return botInfo;
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+            };
+            _botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await _botClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Only process Message updates: https://core.telegram.org/bots/api#message
+            if (update.Message is not { } message)
+                return;
+
+            // Only process text messages
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            // Echo received message text
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+
+        private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
         }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,5 @@
 // Create a new bot instance
 var metBot = new BotEngine(AccessTokens.Telegram);
 
-// Verify the bot is working
-var checkBot = await metBot.CheckBotAsync();
-Console.WriteLine($"I am bot {checkBot.Id} and my name is {checkBot.FirstName}");
+// Listen for messages sent to the bot
+await metBot.ListenForMessages();
